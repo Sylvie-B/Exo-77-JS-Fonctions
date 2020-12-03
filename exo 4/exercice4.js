3516cf0d4cce5546ea58f588174d6da2109b0b48
Sylvie-B <unsolo@hotmail.fr>
2020-12-03 11:29:04 +0100

    insere du texte dans container

--- exo 4/exercice4.js
+++ exo 4/exercice4.js
@@ -1,10 +1,11 @@
 /* Ecrire ci dessous une fonction permettant d'ajouter du texte dans le div appelé container */
-
+function txtInsert() {
+    let newText = document.getElementById("container");
+    newText.prepend("J'ajoute du texte");
+}
 
 
 /* Appeler la fonction */
 
-
-
-
+txtInsert();
 
